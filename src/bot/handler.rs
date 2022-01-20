a4330eb715e97985c1321c281a7fe4323661a414
Vichukano <vichukano@gmail.com>
2022-01-20 10:08:36 +0300

    some refactoring

--- src/bot/handler.rs
+++ src/bot/handler.rs
@@ -36,129 +36,137 @@ impl MessageHandler {
         answer
     }
 
-    fn handle_text(&self, text: &String, mut user: User) -> String {
-        let store = &self.store;
-        let user_info;
+    fn handle_text(&self, text: &String, user: User) -> String {
         let command = text_to_commant(text);
         let bot_answer = match command {
             MessageType::BotCommand(text) => {
-                let answer = match text.as_str() {
-                    "/help" => "Type /start for start training",
-                    "/start" => {
-                        user.set_state(State::SelectExercise);
-                        store.insert(user.id(), user);
-                        "Start training, select exercise from:\n
-                        /bench_press\n
-                        /squat\n
-                        /dead_lift\n
-                        "
-                    }
-                    "/bench_press" => {
-                        let ans = match user.state() {
-                            State::SelectExercise => {
-                                user.set_state(State::SelectWeight);
-                                let exercise = Exercise::new(text.clone());
-                                user.training().add_exercise(exercise);
-                                store.insert(user.id(), user);
-                                "Select bench_press"
-                            }
-                            _ => "Can not select exersice now",
-                        };
-                        ans
-                    }
-                    "/squat" => {
-                        let ans = match user.state() {
-                            State::SelectExercise => {
-                                user.set_state(State::SelectWeight);
-                                let exercise = Exercise::new(text.clone());
-                                user.training().add_exercise(exercise);
-                                store.insert(user.id(), user);
-                                "Select squat"
-                            }
-                            _ => "Can not select exersice now",
-                        };
-                        ans
-                    }
-                    "/dead_lift" => {
-                        let ans = match user.state() {
-                            State::SelectExercise => {
-                                user.set_state(State::SelectWeight);
-                                let exercise = Exercise::new(text.clone());
-                                user.training().add_exercise(exercise);
-                                store.insert(user.id(), user);
-                                "Select dead_lift"
-                            }
-                            _ => "Can not select exersice now",
-                        };
-                        ans
-                    }
-                    "/exercise" => {
-                        let ans = match user.state() {
-                            State::SelectWeight => {
-                                user.set_state(State::SelectExercise);
-                                store.insert(user.id(), user);
-                                "Start training, select exercise from:\n
-                                /bench_press\n
-                                /squat\n
-                                /dead_lift"
-                            }
-                            _ => "can not select exersice now",
-                        };
-                        ans
-                    }
-                    "/show" => {
-                        user_info = format!("User: {:?}", user);
-                        user_info.as_str()
-                    }
-                    "/stop" => {
-                        let training_time = user.training().get_start_time().elapsed();
-                        user_info = format!(
-                            "Finish trainig\nTraining duration: {:?}\nTraining info: {:?}",
-                            training_time.clone(),
-                            user.training().clone()
-                        );
-                        store.remove(&user.id());
-                        user_info.as_str()
-                    }
-                    _ => "Unknown command, try /help",
-                };
-                answer.to_string()
+                let answer = self.handle_bot_command(&text, user);
+                answer
             }
             MessageType::Number(num) => {
-                let ans = match user.state() {
-                    State::SelectWeight => {
-                        user.set_state(State::SelectReps);
-                        match user.training().get_exercises().last_mut() {
-                            Some(ex) => {
-                                log::info!("Add weight: {}", num);
-                                ex.set_weight(num);
-                                store.insert(user.id(), user);
-                                format!("Weight selected: {}, now select reps", num)
-                            }
-                            None => "Exercise not found".to_string(),
-                        }
-                    }
-                    State::SelectReps => {
-                        user.set_state(State::SelectWeight);
-                        match user.training().get_exercises().last_mut() {
-                            Some(ex) => {
-                                log::info!("Add reps: {}", num);
-                                ex.set_reps(num);
-                                store.insert(user.id(), user);
-                                format!("Reps selected: {}, now select weight for next turn, or select /exercise for select another one", num)
-                            }
-                            None => "Exercise not found".to_string(),
-                        }
-                    }
-                    _ => "can not process request".to_string(),
-                };
-                ans
+                let answer = self.handle_bot_digit_command(num, user);
+                answer
             }
             MessageType::Other(text) => {
                 format!("Unknown command: {} start command with /", text)
             }
         };
         String::from(bot_answer)
+    }
+
+    fn handle_bot_command(&self, text: &String, mut user: User) -> String {
+        let store = &self.store;
+        let answer = match text.as_str() {
+            "/help" => "Type /start for start training".to_string(),
+            "/start" => {
+                user.set_state(State::SelectExercise);
+                store.insert(user.id(), user);
+                "Start training, select exercise from:\n
+                /bench_press\n
+                /squat\n
+                /dead_lift\n
+                ".to_string()
+            }
+            "/bench_press" => {
+                let ans = match user.state() {
+                    State::SelectExercise => {
+                        user.set_state(State::SelectWeight);
+                        let exercise = Exercise::new(text.clone());
+                        user.training().add_exercise(exercise);
+                        store.insert(user.id(), user);
+                        "Select bench_press".to_string()
+                    }
+                    _ => "Can not select exersice now".to_string(),
+                };
+                ans
+            }
+            "/squat" => {
+                let ans = match user.state() {
+                    State::SelectExercise => {
+                        user.set_state(State::SelectWeight);
+                        let exercise = Exercise::new(text.clone());
+                        user.training().add_exercise(exercise);
+                        store.insert(user.id(), user);
+                        "Select squat".to_string()
+                    }
+                    _ => "Can not select exersice now".to_string(),
+                };
+                ans
+            }
+            "/dead_lift" => {
+                let ans = match user.state() {
+                    State::SelectExercise => {
+                        user.set_state(State::SelectWeight);
+                        let exercise = Exercise::new(text.clone());
+                        user.training().add_exercise(exercise);
+                        store.insert(user.id(), user);
+                        "Select dead_lift".to_string()
+                    }
+                    _ => "Can not select exersice now".to_string(),
+                };
+                ans
+            }
+            "/exercise" => {
+                let ans = match user.state() {
+                    State::SelectWeight => {
+                        user.set_state(State::SelectExercise);
+                        store.insert(user.id(), user);
+                        "Start training, select exercise from:\n
+                        /bench_press\n
+                        /squat\n
+                        /dead_lift".to_string()
+                    }
+                    _ => "can not select exersice now".to_string(),
+                };
+                ans
+            }
+            "/show" => {
+                format!("User: {:?}", user)
+            }
+            "/stop" => {
+                let training_time = user.training().get_start_time().elapsed();
+                let info = format!(
+                    "Finish trainig\nTraining duration: {:?}\nTraining info: {:?}",
+                    training_time.clone(),
+                    user.training().clone()
+                );
+                store.remove(&user.id());
+                info
+            }
+            _ => "Unknown command, try /help".to_string(),
+        };
+        answer
+    }
+
+    fn handle_bot_digit_command(&self, num: u32, mut user: User) -> String {
+        let ans = match user.state() {
+            State::SelectWeight => {
+                user.set_state(State::SelectReps);
+                match user.training().get_exercises().last_mut() {
+                    Some(ex) => {
+                        log::info!("Add weight: {}", num);
+                        ex.set_weight(num);
+                        self.store.insert(user.id(), user);
+                        format!("Weight selected: {}, now select reps", num)
+                    }
+                    None => "Exercise not found".to_string(),
+                }
+            }
+            State::SelectReps => {
+                user.set_state(State::SelectWeight);
+                match user.training().get_exercises().last_mut() {
+                    Some(ex) => {
+                        log::info!("Add reps: {}", num);
+                        ex.set_reps(num);
+                        self.store.insert(user.id(), user);
+                        format!("Reps selected: {}, now select weight for next turn, or select /exercise for select another one", num)
+                    }
+                    None => "Exercise not found".to_string(),
+                }
+            }
+            _ => "can not process request".to_string(),
+        };
+        ans
     }
 }
 
